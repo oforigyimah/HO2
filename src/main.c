747e1061222b05b4a1f62318e800d208385c6334
Rabbi Ofori Gyimah <rabbioforigyimah@gmail.com>
2024-02-05 16:23:23 +0000

    submodule sha2 remove

--- src/main.c
+++ src/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <curl/curl.h>
 
-
 int main() {
     printf("Hello, World!\n");
 
     curl_version_info_data* data = curl_version_info(CURLVERSION_NOW);
     printf("Curl version: %s\n", data->version);
+
     return 0;
 }
